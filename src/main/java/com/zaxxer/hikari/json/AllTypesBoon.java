3f68ed322081f347c868f77a90ebd1aed5d0eb17
Brett Wooldridge <brett.wooldridge@gmail.com>
2014-09-17 00:08:51 +0900

    Add AllTypes benchmark.

---
+++ src/main/java/com/zaxxer/hikari/json/AllTypesBoon.java
@@ -0,0 +1,54 @@
+package com.zaxxer.hikari.json;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.sql.SQLException;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.io.IOUtils;
+import org.boon.json.JsonParserAndMapper;
+import org.boon.json.JsonParserFactory;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+
+import com.zaxxer.hikari.json.obj.AllType;
+
+
+@BenchmarkMode(Mode.Throughput)
+@OutputTimeUnit(TimeUnit.SECONDS)
+@State(Scope.Benchmark)
+public class AllTypesBoon
+{
+	private byte[] input;
+	private final JsonParserAndMapper mapper = new JsonParserFactory ().create ();
+	
+	@Setup
+    public void setup()
+    {
+		File file = new File("src/test/resources/AllTypes.json");
+		try (InputStream is = new FileInputStream(file)) {
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			IOUtils.copy(is, baos);
+			baos.close();
+			input = baos.toByteArray();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+    }
+
+	@Benchmark
+    public AllType parseMenu() throws SQLException
+    {
+		return mapper.parse(AllType.class, new ByteArrayInputStream(input), Charset.defaultCharset());
+    }
+}
---
+++ src/main/java/com/zaxxer/hikari/json/AllTypesHikari.java
@@ -0,0 +1,52 @@
+package com.zaxxer.hikari.json;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.io.IOUtils;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+
+import com.zaxxer.hikari.json.JsonFactory.Option;
+import com.zaxxer.hikari.json.obj.AllType;
+
+
+@BenchmarkMode(Mode.Throughput)
+@OutputTimeUnit(TimeUnit.SECONDS)
+@State(Scope.Benchmark)
+public class AllTypesHikari
+{
+	private byte[] input;
+	private final static ObjectMapper objectMapper = JsonFactory.option(Option.FIELD_ACCESS, Option.CONSISTENT_STRUCTURE, Option.MEMBERS_ASCII, Option.VALUES_ASCII).create();
+
+	@Setup
+    public void setup()
+    {
+		File file = new File("src/test/resources/AllTypes.json");
+		try (InputStream is = new FileInputStream(file)) {
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			IOUtils.copy(is, baos);
+			baos.close();
+			input = baos.toByteArray();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+    }
+
+	@Benchmark
+    public AllType parseMenu() throws SQLException
+    {
+		return objectMapper.readValue(new ByteArrayInputStream(input), AllType.class);
+    }
+}
